900d32eb42261f47206bf28ad403155f14dd28ec
wandji20 <wbertrand10@gmail.com>
2021-02-06 15:34:56 +0100

    Update sign_in helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def my_sign_in
-    if member_signed_in?
-      '/layouts/signed_in'
-    else
-      '/layouts/signed_out'
-    end
+    member_signed_in? ? '/layouts/signed_in' : '/layouts/signed_out' 
   end
 end
 
