823020688af61af801ddfb6add7bcc0925f4ace7
wandji20 <wbertrand10@gmail.com>
2021-02-06 14:34:25 +0100

    Update views to display post authur when signed in

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_member.posts.build
   end
 
   # GET /posts/1/edit
@@ -22,7 +22,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_member.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:post)
     end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
-class RegistrationController < Devise::RegistrationController
+class RegistrationsController < Devise::RegistrationsController
 
   private
 
   def sign_up_params
-    params.require(:member).permit(:name, :email, :password, :password_confirmation)
+    params.require(:member).permit(:name, :member_name, :email, :password, :password_confirmation)
   end
 
 end
